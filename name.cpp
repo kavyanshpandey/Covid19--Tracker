a989fadbdc954ac39495810acbd57f446d75c695
ar-chit <72187362+ar-chit@users.noreply.github.com>
2021-10-02 11:34:56 +0530

    Create name.cpp
    accept please

---
+++ name.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string name;
+    cout<<"Enter your name"<<endl;
+    getline(cin,name);
+    cout<<"Welcome @"<<name;
+    return 0;
+}     
