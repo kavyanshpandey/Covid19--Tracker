ce3380bb37fa738b474244e54151644b8d250cfc
sa8tyajeet46 <satyajeettripathy677@gmail.com>
2022-10-02 00:43:09 +0530

    comment added to main.js

--- src/main.js
+++ src/main.js
@@ -1,33 +1,34 @@
-import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
 
-import App from './App.vue';
-import Home from './components/Home';
-import WelcomeScreen from './components/WelcomScreen';
-
-
-
+import App from "./App.vue";
+import Home from "./components/Home";
+import WelcomeScreen from "./components/WelcomScreen";
 
 const router = createRouter({
-    history:createWebHistory(),
-    routes: [
-        {
-            path:'/', component:WelcomeScreen
-        },
-        {
-            path:'/stats', component:Home
-        },
-        {
-            path:'/:notFound(.*)', redirect:'/'
-        }
-    ]
-})
+  history: createWebHistory(),
+  routes: [
+    {
+      //path to home
+      path: "/",
+      component: WelcomeScreen,
+    },
+    {
+      //path to stats
+      path: "/stats",
+      component: Home,
+    },
+    {
+      //path for 404
+      path: "/:notFound(.*)",
+      redirect: "/",
+    },
+  ],
+});
 const app = createApp(App);
-
-
 
 app.use(router);
 
-app.mount('#app');
+app.mount("#app");
 
 //keep the good work going
